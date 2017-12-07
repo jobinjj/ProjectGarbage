0ba71712d7448faaf97492e14ecf587d0f222394
jobinjj <jobinj1048@gmail.com>
2017-12-07 12:52:47 -0800

    checklist

--- app/src/main/java/sample/com/jobin/msi/projectgarbage/KarangEditProfile.java
+++ app/src/main/java/sample/com/jobin/msi/projectgarbage/KarangEditProfile.java
@@ -2,15 +2,26 @@ package sample.com.jobin.msi.projectgarbage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class KarangEditProfile extends AppCompatActivity {
 
     Spinner spinner;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_karang_edit_profile);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.cities_array, R.layout.spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner = findViewById(R.id.spinner);
+
+        spinner.setAdapter(adapter);
     }
 }
